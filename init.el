82ed2f3e2c31c77026dc995b7f1985609f478825
tarrsalah <tarrsalah@gmail.com>
2015-10-22 08:35:18 +0100

    use ubuntu mono font.

--- init.el
+++ init.el
@@ -111,7 +111,7 @@ BUFFER may be either a buffer or its name (a string)."
 (setq dired-omit-files "\\.pdf$\\|\\.pyc$\\|\\.tern-port$")
 
 ;; set font size
-(set-face-attribute 'default nil :height 120)
+(set-face-attribute 'default nil :family "Ubuntu Mono" :height 140)
 
 
 ;;; install first-class packages
