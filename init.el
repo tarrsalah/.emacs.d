4d4069a3b593887f5ea3eb97f7944a70b2c42756
tarrsalah <tarrsalah@gmail.com>
2016-05-04 10:37:54 +0100

    increment number at point.

--- init.el
+++ init.el
@@ -306,31 +306,6 @@ BUFFER may be either a buffer or its name (a string)."
 (use-package geiser
   :ensure t)
 
-;;;  golang
-
-;; go-mode
-(add-to-list 'load-path "~/src/github.com/dominikh/go-mode.el")
-(require 'go-mode-autoloads)
-
-                                        ; gocode
-(add-to-list 'load-path "~/src/github.com/nsf/gocode/emacs-company")
-(require 'company)
-(require 'company-go)
-(add-hook 'go-mode-hook
-          (lambda ()
-            (set (make-local-variable 'company-backends) '(company-go))
-            (company-mode)))
-
-;; golint
-(add-to-list 'load-path
-             (concat (getenv "GOPATH")
-                     "/src/github.com/golang/lint/misc/emacs"))
-(require 'golint)
-
-;; goimports
-;; (setq gofmt-command "goimports")
-(add-hook 'before-save-hook 'gofmt-before-save)
-
 ;; html
 (use-package emmet-mode
   :ensure t
@@ -436,6 +411,9 @@ BUFFER may be either a buffer or its name (a string)."
   :config
     (add-hook 'css-mode-hook  (lambda () (rainbow-mode 1))))
 
+(use-package rainbow-delimiters
+  :ensure t)
+
 ;;; key bindings
 (global-set-key (kbd "C-+") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
@@ -474,3 +452,71 @@ BUFFER may be either a buffer or its name (a string)."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(grizzl-selection-face ((t (:foreground "#8F9D6A")))))
+
+
+(require 'thingatpt)
+
+(defun thing-at-point-goto-end-of-integer ()
+  "Go to end of integer at point."
+  (let ((inhibit-changing-match-data t))
+    ;; Skip over optional sign
+    (when (looking-at "[+-]")
+      (forward-char 1))
+    ;; Skip over digits
+    (skip-chars-forward "[[:digit:]]")
+    ;; Check for at least one digit
+    (unless (looking-back "[[:digit:]]")
+      (error "No integer here"))))
+(put 'integer 'beginning-op 'thing-at-point-goto-end-of-integer)
+
+(defun thing-at-point-goto-beginning-of-integer ()
+  "Go to end of integer at point."
+  (let ((inhibit-changing-match-data t))
+    ;; Skip backward over digits
+    (skip-chars-backward "[[:digit:]]")
+    ;; Check for digits and optional sign
+    (unless (looking-at "[+-]?[[:digit:]]")
+      (error "No integer here"))
+    ;; Skip backward over optional sign
+    (when (looking-back "[+-]")
+      (backward-char 1))))
+(put 'integer 'beginning-op 'thing-at-point-goto-beginning-of-integer)
+
+(defun thing-at-point-bounds-of-integer-at-point ()
+  "Get boundaries of integer at point."
+  (save-excursion
+    (let (beg end)
+      (thing-at-point-goto-beginning-of-integer)
+      (setq beg (point))
+      (thing-at-point-goto-end-of-integer)
+      (setq end (point))
+      (cons beg end))))
+(put 'integer 'bounds-of-thing-at-point 'thing-at-point-bounds-of-integer-at-point)
+
+(defun thing-at-point-integer-at-point ()
+  "Get integer at point."
+  (let ((bounds (bounds-of-thing-at-point 'integer)))
+    (string-to-number (buffer-substring (car bounds) (cdr bounds)))))
+(put 'integer 'thing-at-point 'thing-at-point-integer-at-point)
+
+(defun increment-integer-at-point (&optional inc)
+  "Increment integer at point by one.
+
+With numeric prefix arg INC, increment the integer by INC amount."
+  (interactive "p")
+  (let ((inc (or inc 1))
+        (n (thing-at-point 'integer))
+        (bounds (bounds-of-thing-at-point 'integer)))
+    (delete-region (car bounds) (cdr bounds))
+    (insert (int-to-string (+ n inc)))))
+
+(defun decrement-integer-at-point (&optional dec)
+  "Decrement integer at point by one.
+
+With numeric prefix arg DEC, decrement the integer by DEC amount."
+  (interactive "p")
+  (increment-integer-at-point (- (or dec 1))))
+
+
+(global-set-key (kbd "C-c C-<up>") 'increment-integer-at-point)
+(global-set-key (kbd "C-c C-<down>") 'decrement-integer-at-point)
